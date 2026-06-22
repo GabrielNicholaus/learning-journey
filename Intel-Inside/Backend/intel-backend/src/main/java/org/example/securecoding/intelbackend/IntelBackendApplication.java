cfdadee3597072c1c842a85dd7381442be49d6b8
GabrielNicholaus <gabnico19@gmail.com>
2026-06-22 12:51:54 +0700

    Add files via upload

---
+++ Intel-Inside/Backend/intel-backend/src/main/java/org/example/securecoding/intelbackend/IntelBackendApplication.java
@@ -0,0 +1,12 @@
+package org.example.securecoding.intelbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelBackendApplication.class, args);
+    }
+}
